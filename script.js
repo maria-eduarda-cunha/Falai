8c9b5f2996cce47885854dde3b64fad2a076cd04
Maria Eduarda Cunha <128634212+maria-eduarda-cunha@users.noreply.github.com>
2025-10-19 20:04:16 +0000

    add: aba 'Minhas Denúncias'

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// --------------------- TELAS ---------------------
 function mostrarTela(telaId, menuItem) {
   // Esconde todas as telas
   const telas = document.querySelectorAll('section');
@@ -15,3 +16,26 @@ function mostrarTela(telaId, menuItem) {
 
 // Mostra a tela de mapa por padrão
 mostrarTela('tela-mapa', document.querySelector('.menu a'));
+
+
+// --------------------- TELA LISTA DENUNCIAS ---------------------
+const abas = document.querySelectorAll('.lista-denuncias-menu .aba');
+const denuncias = document.querySelectorAll('.lista-denuncias .denuncia');
+
+function filtrarDenuncias(status) {
+  denuncias.forEach(d => {
+    d.style.display = d.dataset.status === status ? 'block' : 'none';
+  });
+}
+
+// Inicializa com “Abertas” visíveis
+filtrarDenuncias('abertas');
+
+abas.forEach(aba => {
+  aba.addEventListener('click', () => {
+    abas.forEach(a => a.classList.remove('active'));
+    aba.classList.add('active');
+
+    filtrarDenuncias(aba.dataset.status);
+  });
+});
